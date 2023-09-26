da99f38e56818867c952c7d9210d675de25a908a
AlanShelbyCode <abiturientweselski@gmail.com>
2023-09-26 22:26:39 +0300

    task3-books

--- Controller.py
+++ Controller.py
@@ -1,30 +1,30 @@
-from models.models import User
+from models.models import Book
 class Сontroller:
 
     def __init__(self):
         pass
     def menu(self):
         while True:
-            print("1. Add New User\n"
-                   + "2. Get All Users\n"
-                   + "3. Search\n"
-                   + "4. Update User By Id"
+            print("1. Add New Book\n"
+                   + "2. Get All Books\n"
+                   + "3. Search Book by\n"
+                   + "4. Update Book By Id"
                   )
             menu_flag = int(input("Type your choose: "))
             if menu_flag == 1:
-                self.add_user()
+                self.add_book()
             elif menu_flag == 2:
-                User.get_all_users()
+                Book.get_all_books()
             elif menu_flag == 3:
-                what_to_search = input('By Which Parametr you want to search: ')
+                what_to_search = input('By Which Parametr you want to search '
+                                       '(id/book_name/book_autor): ')
                 search_str = input('Search: ')
-                User.search_by(search_str, what_to_search)
+                Book.search_by(search_str, what_to_search)
             elif menu_flag == 4:
-                User.update_user()
+                Book.update_book()
 
-    def add_user(self):
-        first_name = input("First name: ")
-        last_name = input("Last name: ")
-        email = input("Email: ")
-        user = User(first_name, last_name, email)
-        user.save()
+    def add_book(self):
+        book_name = input("Title of book: ")
+        book_autor = input("Last name: ")
+        book = Book(book_name,book_autor)
+        book.save()
--- models/models.py
+++ models/models.py
@@ -1,68 +1,52 @@
 from farmework.models import Model
 import json
 
-class User(Model):
-    file = 'users.json'
+class Book(Model):
+    file = 'books.json'
 
-    def __init__(self, first_name, last_name, email):
+    def __init__(self, book_name, book_autor):
 
         data = self.get_file_data(self.file)
         if len(data) > 0:
             self.id = data[-1]['id'] + 1
         else:
             self.id = 1
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        if self.check_email() == True:
-            print('User with this email already exist!!!')
-            self.email = None
-
-    def check_email(self):
-        data = self.get_file_data(self.file)
-        for user in data:
-            if self.email == user['email']:
-                return True
-        return False
+        self.book_name = book_name
+        self.book_autor = book_autor
 
     #думав зробити лист, але вирішив не змінювати
     @classmethod
-    def get_all_users(cls):
+    def get_all_books(cls):
         with open('database/'+cls.file, 'r') as file:
-            users = json.loads(file.read())
-            for user in users:
-                print("User #" + str(user['id']))
-                print("First Name: " + user['first_name'])
-                print("Last Name: " + user['last_name'])
-                print("Email: " + str(user['email']))
-
+            books = json.loads(file.read())
+            for book in books:
+                print("Book #" + str(book['id']))
+                print("First Name: " + book['book_name'])
+                print("Last Name: " + book['book_autor'])
     @classmethod
     def search_by(cls, search_str, what_to_search):
-             with open('database/'+cls.file, 'r') as file:
-                users = json.loads(file.read())
-                for user in users:
-                    if str(user[what_to_search]).lower() == str(search_str).lower():
-                        print("User #" + str(user['id']))
-                        print("First Name: " + user['first_name'])
-                        print("Last Name: " + user['last_name'])
-                        print("Email: " + str(user['email']))
+        with open('database/' + cls.file, 'r') as file:
+            books = json.loads(file.read())
+            for book in books:
+                    if str(book[what_to_search]).lower() == str(search_str).lower():
+                        print("Book #" + str(book['id']))
+                        print("First Name: " + book['book_name'])
+                        print("Last Name: " + book['book_autor'])
                     else:
                         print('Not found')
 
     @classmethod
-    def update_user(cls):
+    def update_book(cls):
         file = open('database/'+cls.file, 'r')
-        users = json.loads(file.read())
+        books = json.loads(file.read())
         file.close()
-        id = int(input("Type id of user which you want to update: "))
-        first_name = input("First Name: ")
-        last_name = input("Last Name: ")
-        email = input("Email: ")
-        for user in users:
-            if user['id'] == id:
-                user['first_name'] = first_name
-                user['last_name'] = last_name
-                user['email'] = email
+        id = int(input("Type id of book which you want to update: "))
+        book_name = input("Book title: ")
+        book_autor = input("Book autor: ")
+        for book in books:
+            if book['id'] == id:
+                book['first_name'] = book_name
+                book['last_name'] = book_autor
 
         with open('database/'+cls.file, 'w') as file:
-            file.write(json.dumps(users))
+            file.write(json.dumps(books))
